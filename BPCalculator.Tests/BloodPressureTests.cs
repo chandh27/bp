3c7d19514fde993efc5c27f3077be4ae31bf2e0d
chan27052705 <chandini13@gmail.com>
2025-11-21 21:20:55 +0530

    added unit-test case

--- BPCalculator.Tests/BloodPressureTests.cs
+++ BPCalculator.Tests/BloodPressureTests.cs
@@ -4,38 +4,47 @@ namespace BPCalculator.Tests
 {
     public class BloodPressureTests
     {
+        // Category checking tests
         [Theory]
-        [InlineData(85,55,BPCategory.Low)]
-        [InlineData(110,70,BPCategory.Ideal)]
-        [InlineData(130,85,BPCategory.PreHigh)]
-        [InlineData(150,95,BPCategory.High)]
+        [InlineData(85, 55, BPCategory.Low)]
+        [InlineData(110, 70, BPCategory.Ideal)]
+        [InlineData(130, 85, BPCategory.PreHigh)]
+        [InlineData(150, 95, BPCategory.High)]
+
         public void Calculates_Correct_Category(int sys, int dia, BPCategory expected)
         {
             var bp = new BloodPressure { Systolic = sys, Diastolic = dia };
             Assert.Equal(expected, bp.Category);
         }
-//test1
+
         [Fact]
         public void Throws_When_Diastolic_GTE_Systolic()
         {
             var bp = new BloodPressure { Systolic = 100, Diastolic = 120 };
             Assert.Throws<ArgumentOutOfRangeException>(() => bp.Category);
         }
-//test2
+
         [Fact]
         public void Throws_When_Systolic_Below_Min()
         {
             var bp = new BloodPressure { Systolic = 60, Diastolic = 70 };
             Assert.Throws<ArgumentOutOfRangeException>(() => bp.Category);
         }
-//test3
+
         [Fact]
         public void Throws_When_Systolic_Above_Max()
         {
-            var bp = new BloodPressure { Systolic = 200, Diastolic = 70 };
+            var bp = new BloodPressure { Systolic = 200, Diastolic = 80 };
             Assert.Throws<ArgumentOutOfRangeException>(() => bp.Category);
         }
-//test4
+
+        [Fact]
+        public void Throws_When_Diastolic_Below_Min()
+        {
+            var bp = new BloodPressure { Systolic = 100, Diastolic = 30 };
+            Assert.Throws<ArgumentOutOfRangeException>(() => bp.Category);
+        }
+
         [Fact]
         public void Throws_When_Diastolic_Above_Max()
         {
@@ -44,5 +53,11 @@ namespace BPCalculator.Tests
         }
 
 
+        [Fact]
+        public void PreHigh_When_Diastolic_Triggers()
+        {
+            var bp = new BloodPressure { Systolic = 118, Diastolic = 85 };
+            Assert.Equal(BPCategory.PreHigh, bp.Category);
+        }
     }
 }
